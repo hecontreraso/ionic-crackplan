2f167a832ea371562ae532e050db6dbce13e9182
Esteban <hecontreraso@gmail.com>
2015-09-01 15:32:28 -0500

    Improvements in feeds page. Now only needs to add some privacy

--- www/js/controllers/events-controller.js
+++ www/js/controllers/events-controller.js
@@ -3,6 +3,7 @@ angular.module('EventsController', [])
 .controller('EventsCtrl', function($scope, $http, $timeout, EventService) {
 	$scope.events = [];
 	$scope.moreDataCanBeLoaded = true;
+	$scope.limit_assistants_shown = 5;
 	var event_index = 0;
 
 	$scope.loadMore = function() {
@@ -11,6 +12,9 @@ angular.module('EventsController', [])
         $scope.moreDataCanBeLoaded = false;
       }
 
+      events = formatEvents(events);
+			console.log(events[0]);
+
 			$scope.events = $scope.events.concat(events);
 			event_index += events.length;
 			$scope.$broadcast('scroll.infiniteScrollComplete');
@@ -18,6 +22,32 @@ angular.module('EventsController', [])
 	}
 
 	$scope.toggleAssistance = function(event){
-		event.user_is_going = EventService.toggle_assistance(event);
+		EventService.toggleAssistance(event).then(function(user_is_going){
+			event.user_is_going = user_is_going;
+			event.going_or_join_label = event.user_is_going ? 'Going' : 'Join';
+		});
 	}
+
+	function formatEvents(events){
+		events.forEach(function(event){
+			if(event.feed_creator.image === null){
+				event.feed_creator.image = '../img/profile_missing.png';
+			}
+			if(event.image === null){
+				event.image = '../img/event_missing.jpg';
+			}
+			event.going_or_join_label = event.user_is_going ? 'Going' : 'Join';
+
+			var total_assistants = event.assistants.length;
+	    event.all_but_last_two_assistants = event.assistants.slice(0, total_assistants - 2);
+	    event.last_two_assistants = event.assistants.slice(total_assistants - 2);
+
+	    event.showed_assistants = event.assistants.slice(0, $scope.limit_assistants_shown);
+	    event.hidden_assistants_qty = event.assistants.length - $scope.limit_assistants_shown;
+	    // event.hidden_assistants = event.assistants.slice(limit_assistants_shown, total_assistants - $scope.limit_assistants_shown);
+
+		});
+		return events;
+	}
+
 });
--- www/js/services/events-service.js
+++ www/js/services/events-service.js
@@ -10,19 +10,10 @@ angular.module('EventService', [])
 				return response.data;
 			});
 		},
-		// GetNewEvents: function(){
-		// 	return $http.get(SERVER_URL + '/events/10').then(function(response){
-		// 		events = response.data;
-		// 		return events;
-		// 	});
-		// },
 		toggleAssistance: function(event){
-			$http.post(SERVER_URL + '/events/' + event.id + '/toggle_assistance/')
-			.success(function(data){
-				return data.user_is_going;
-			})
-			.error(function(){
-				console.log("Error when changing assistance");
+			return $http.post(SERVER_URL + '/events/' + event.id + '/toggle_assistance/')
+			.then(function(response){
+				return response.data.user_is_going;
 			});
 		}
 	}
